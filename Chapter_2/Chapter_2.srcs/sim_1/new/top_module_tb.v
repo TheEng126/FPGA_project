1e17731d34f758280b8d79ee10cdb440119603e9
TheAnh <theanhslby@gmail.com>
2025-08-30 16:30:06 +0700

    update

--- Chapter_2/Chapter_2.srcs/sim_1/new/top_module_tb.v
+++ Chapter_2/Chapter_2.srcs/sim_1/new/top_module_tb.v
@@ -4,7 +4,6 @@ module top_module_tb;
     reg [3:0] b;
     wire out;
 
-    // Gọi module cần test (DUT = Device Under Test)
     top_module uut (
         .a(a),
         .b(b),
@@ -14,14 +13,14 @@ module top_module_tb;
     integer i, j;
 
     initial begin
-        // Duyệt hết các giá trị của a và b (0 đến 15)
+
         for (i = 0; i < 16; i = i + 1) begin
             for (j = 0; j < 16; j = j + 1) begin
                 a = i[3:0];
                 b = j[3:0];
-                #10; // chờ 10ns cho mỗi vector
+                #10; 
             end
         end
-        $stop; // dừng simulation
+        $stop;
     end
 endmodule
